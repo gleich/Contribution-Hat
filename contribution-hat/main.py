39845474eb7f29be373acf38f1eb32d56e12ae83
Matthew Gleich <matthewgleich@gmail.com>
2020-02-29 14:49:33 -0500

    Create working version

--- contribution-hat/main.py
+++ contribution-hat/main.py
@@ -8,7 +8,7 @@ with open("./contribution-hat-config/username.txt") as username_file:
     username = username_file.read()
 
 while True:
-    if datetime.now().hour < 6:
+    if False:
         clear_vals = []
         for i in range(64):
             clear_vals.append([0, 0, 0])
@@ -36,17 +36,24 @@ while True:
             date = date.today() - timedelta(i)
             contributions = response["data"][str(
                 date.year)][str(date.month)][str(date.day)]
-            percentage_of_max = max_contributions / contributions
-            if percentage_of_max == 0:
-                values.append([250, 250, 250])
-            elif percentage_of_max < 25:
-                values.append([198, 228, 139])
-            elif percentage_of_max < 50:
-                values.append([123, 201, 111])
-            elif percentage_of_max < 75:
-                values.append([35, 154, 59])
+            if contributions != 0:
+                percentage_of_max = contributions / max_contributions
             else:
-                values.append([25, 97, 39])
+                percentage_of_max = 0
+            print("{}/{}/{}".format(date.month, date.day, date.year))
+            print("\tPercentage of max", percentage_of_max)
+            print("\tIndex", i)
+            print("\t Contributions", contributions)
+            if percentage_of_max == 0:
+                values.append([255, 0, 0])
+            elif percentage_of_max < 0.50:
+                values.append([252, 248, 0])
+            elif percentage_of_max < 0.75:
+                values.append([0, 255, 0])
+            else:
+                values.append([25, 73, 13])
         with open("./sense_hat_containerized/leds.json", "w") as leds_json:
             json.dump(values, leds_json)
         sleep(10)
+
+RUN AGAIN
