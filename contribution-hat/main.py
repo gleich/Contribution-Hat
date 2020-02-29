a29b3aeb15078fb902a27a3921ffd1f4d7242ea6
Matthew Gleich <matthewgleich@gmail.com>
2020-02-29 14:57:11 -0500

    Sleep at night

--- contribution-hat/main.py
+++ contribution-hat/main.py
@@ -8,13 +8,13 @@ with open("./contribution-hat-config/username.txt") as username_file:
     username = username_file.read()
 
 while True:
-    if False:
+    if datetime.now().hour < 7 and datetime.now() > 23:
         clear_vals = []
         for i in range(64):
             clear_vals.append([0, 0, 0])
         with open("./sense_hat_containerized/leds.json", "w") as leds_json:
             json.dump(clear_vals, leds_json)
-        sleep(10)
+        sleep(30)
     else:
         # Making request to get user's contributions
         url = "https://github-contributions-api.herokuapp.com/{}/count".format(
@@ -46,6 +46,8 @@ while True:
             print("\t Contributions", contributions)
             if percentage_of_max == 0:
                 values.append([255, 0, 0])
+            elif percentage_of_max < 0.25:
+                values.append([255, 154, 2])
             elif percentage_of_max < 0.50:
                 values.append([252, 248, 0])
             elif percentage_of_max < 0.75:
@@ -54,6 +56,4 @@ while True:
                 values.append([25, 73, 13])
         with open("./sense_hat_containerized/leds.json", "w") as leds_json:
             json.dump(values, leds_json)
-        sleep(10)
-
-RUN AGAIN
+        sleep(30)
