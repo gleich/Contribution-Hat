517393a946fbd88b86542228efc22e9ab28f5a48
Matthew Gleich <matthewgleich@gmail.com>
2020-02-29 19:52:33 -0500

    Remove any preexisting folders

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+rm -rf ~/sense_hat_containerized
+
 mkdir ~/sense_hat_containerized
 
 touch ~/sense_hat_containerized/data.json
 
 touch ~/sense_hat_containerized/leds.json
+
+rm -rf ~/contribution-hat-config
 
 mkdir ~/contribution-hat-config
 
